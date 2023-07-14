0f3352c29475e98f015e7893ed70dd7bedbd321a
Michele Memoli <michele@100shapes.com>
2023-07-14 22:34:55 +0100

    chore(web): 🔧 more cors BS

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -28,30 +28,30 @@ const nextConfig = {
     NX_DOMAIN: process.env.NX_DOMAIN ?? '',
   },
 
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: process.env.NX_DOMAIN ?? 'https://www.visalytics.org',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: '/api/:path*',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: process.env.NX_DOMAIN ?? 'https://www.visalytics.org',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: 'GET,OPTIONS',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value:
+  //             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 
   // experimental: {
   //   outputFileTracingRoot: path.join(__dirname, '../../'),
--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -1,23 +1,41 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
-
+import { NextApiRequest, NextApiResponse } from 'next';
 import { appRouter, createTRPCContext } from '@visalytics/shared-services/trpc';
+import Cors from 'cors';
 
-// export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
+const cors = Cors({
+  methods: ['OPTIONS', 'GET', 'HEAD'],
+  origin: 'https://www.visalytics.org',
 });
 
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  fn: (...args: any) => unknown
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
 // If you need to enable cors, you can do so like this:
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   // Enable cors
-//   await cors(req, res);
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Enable cors
+  await runMiddleware(req, res, cors);
 
-//   // Let the tRPC handler do its magic
-//   return createNextApiHandler({
-//     router: appRouter,
-//     createContext,
-//   })(req, res);
-// };
+  // Let the tRPC handler do its magic
+  return createNextApiHandler({
+    router: appRouter,
+    createContext: createTRPCContext,
+  })(req, res);
+};
 
-// export default handler;
+export default handler;
