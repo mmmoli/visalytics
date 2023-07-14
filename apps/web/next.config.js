dc4873b53d46c4b64a430556c6c26457a4896262
Michele Memoli <michele@100shapes.com>
2023-07-14 10:04:03 +0100

    refactor(web): ♻️ moved Cors configuration
    Set it as headers in our entire app not just api route

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const path = require('path');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -24,6 +25,48 @@ const nextConfig = {
       process.env.NX_PUBLIC_CLERK_AFTER_SIGN_IN_URL ?? '',
     NX_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
       process.env.NX_PUBLIC_CLERK_AFTER_SIGN_UP_URL ?? '',
+    NX_DOMAIN: process.env.NX_DOMAIN ?? '',
+  },
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: process.env.NX_DOMAIN ?? 'http://localhost:4200',
+          }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/.pnpm/@swc+core-linux-x64-gnu@1.3.51',
+        'node_modules/.pnpm/@swc+core-linux-x64-musl@1.3.51',
+        'node_modules/.pnpm/next@13.4.1_@babel+core@7.22.5_react-dom@18.2.0_react@18.2.0',
+        'node_modules/.pnpm/@esbuild+linux-x64@0.17.19',
+        'node_modules/.pnpm/webpack@5.88.1_@swc+core@1.3.51',
+        'node_modules/.pnpm/react-dom@18.2.0_react@18.2.0',
+        'node_modules/.pnpm/caniuse-lite@1.0.30001509',
+        'node_modules/.pnpm/terser@5.18.2',
+      ],
+    },
   },
 };
 
--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -1,40 +1,23 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
-import { appRouter, createTRPCContext } from '@visalytics/shared-services/trpc';
-import { NextApiRequest, NextApiResponse } from 'next';
-import Cors from 'cors';
 
-const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
+import { appRouter, createTRPCContext } from '@visalytics/shared-services/trpc';
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
 });
 
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  fn: (...params: any) => unknown
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: unknown) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
+// If you need to enable cors, you can do so like this:
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   // Enable cors
+//   await cors(req, res);
 
-      return resolve(result);
-    });
-  });
-}
+//   // Let the tRPC handler do its magic
+//   return createNextApiHandler({
+//     router: appRouter,
+//     createContext,
+//   })(req, res);
+// };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // Run the middleware
-  await runMiddleware(req, res, cors);
-
-  return createNextApiHandler({
-    router: appRouter,
-    createContext: createTRPCContext,
-  })(req, res);
-}
+// export default handler;
