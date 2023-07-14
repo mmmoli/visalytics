2f3a4abe76598c72fd0b2cdb614136b6351c490a
Michele Memoli <michele@100shapes.com>
2023-07-14 09:49:32 +0100

    perf(web): ⚡️ enabled cors
    Needed for clerk apparently

--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -1,23 +1,40 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
-
 import { appRouter, createTRPCContext } from '@visalytics/shared-services/trpc';
+import { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
 
-// export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
 });
 
-// If you need to enable cors, you can do so like this:
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   // Enable cors
-//   await cors(req, res);
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  fn: (...params: any) => unknown
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
 
-//   // Let the tRPC handler do its magic
-//   return createNextApiHandler({
-//     router: appRouter,
-//     createContext,
-//   })(req, res);
-// };
+      return resolve(result);
+    });
+  });
+}
 
-// export default handler;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  return createNextApiHandler({
+    router: appRouter,
+    createContext: createTRPCContext,
+  })(req, res);
+}
